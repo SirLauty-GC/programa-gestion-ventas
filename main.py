09180b81c17314b927346f68d9aa3db919b4df2c
SirLauty-GC <caylalautaro2@gmail.com>
2025-03-20 17:52:24 -0300

    tabla funcionamiento básico

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-from tkinter import Tk, Listbox, Button
+from tkinter import Tk, Listbox, Button, Entry, END
 from tkinter import ttk
 import var_glob
 
 class Main():
     def __init__(self):
-        self.fuente_de_texto_berlin_sans_fb = ("Berlin Sans FB", 20)
+        self.fuente_berlin_sans_fb_10 = ("Berlin Sans FB", 10)
+        self.fuente_berlin_sans_fb_20 = ("Berlin Sans FB", 20)
         self.root = Tk()
         self.root.geometry(var_glob.ventana_tamaño)
         self.ventana = ttk.Frame(self.root)
-        self.ventana.grid()
+        self.ventana.pack(fill="both", expand=True)
 
         self.elementos_de_la_ventana()
         self.root.mainloop()
     
     def elementos_de_la_ventana(self):
-        self.elemento_tabla_de_datos()
+        self.elemento_campo_de_texto_ingresar()
         self.elemento_boton_ingresar()
+        self.elemento_boton_borrar()
+        self.elemento_tabla_de_datos()
 
+    # Elementos, botones y campos
+    
     def elemento_tabla_de_datos(self):
-        self.tabla_principal = Listbox(self.ventana,
-                                       bg= "#202020",
-                                       fg="#FFFFFF",
-                                       font=self.fuente_de_texto_berlin_sans_fb)
+        columnas = ("ID", "Nombre", "Valor")
+
+        self.tabla_principal = ttk.Treeview(self.ventana, columns= columnas, show="headings")
+        for col in columnas: # Encabezado
+            self.tabla_principal.heading(col, text=col)
+            self.tabla_principal.column(col, width=100)
+
+        datos = [(1,"Nombre 1", 17),(2,"Nombre 2", 17)]
+        for fila in datos:
+            self.tabla_principal.insert("", END, values=fila)
         self.tabla_principal.pack()
-        self.tabla_principal.insert(1, "Pizza")
-
+        
     def elemento_boton_ingresar(self):
-        boton_ingresar = Button(self.ventana,bg="#2F71D5", text="Ingresar", command= self.comando_ingresar)
+        boton_ingresar = Button(self.ventana,bg="#2F71D5", text="Ingresar", command= self.comando_ingresar, font=self.fuente_berlin_sans_fb_10)
         boton_ingresar.pack()
+        
+    def elemento_boton_borrar(self):
+        boton_borrar = Button(self.ventana,bg="#2F71D5", text="Borrar", command= self.comando_borrar, font=self.fuente_berlin_sans_fb_10)
+        boton_borrar.pack()
 
+    def elemento_campo_de_texto_ingresar(self):
+        self.campo_de_texto_ingresar = Entry(self.ventana)
+        self.campo_de_texto_ingresar.pack()
+
+    # Comandos a ejeutar
     def comando_ingresar(self):
-        print(self.tabla_principal.get(self.tabla_principal.curselection()))
+        nuevo_id = len(self.tabla_principal.get_children()) + 1
+        nuevo_nombre = self.campo_de_texto_ingresar.get()
+        self.tabla_principal.insert("", END, values=(nuevo_id, nuevo_nombre, "N/A"))
+
+    def comando_borrar(self):
+        selected_item = self.tabla_principal.selection()
+        if selected_item:
+            self.tabla_principal.delete(selected_item)
 
 if __name__ == "__main__":
     programa = Main()
